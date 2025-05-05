19ab65a0963383e4f8a3b640b741e748e344b392
kusalya <kusalyawidanagama@gmail.com>
2025-05-06 00:24:35 +0530

    Update client ap32

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,33 @@
-import React from 'react';
+// client/src/App.jsx
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import Navbar from './components/NavBar';
-import Home from './pages/Home';
-import Country from './pages/Country';
-import Favorites from './pages/Favorites';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import { AuthProvider }    from './context/AuthContext';
+import Navbar              from './components/NavBar';
+import Home                from './pages/Home';
+import Country             from './pages/Country';
+import Favorites           from './pages/Favorites';
+import Login               from './pages/Login';
+import Register            from './pages/Register';
 
+export default function App() {
+  useEffect(() => {
+    console.log('🚀 API_BASE_URL =', import.meta.env.VITE_API_BASE_URL);
+  }, []);
 
-function App() {
-  
   return (
     <AuthProvider>
       <Router>
         <Navbar />
         <div className="container mx-auto p-4">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/"         element={<Home />} />
             <Route path="/country/:code" element={<Country />} />
-            <Route path="/favorites" element={<Favorites />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/favorites"     element={<Favorites />} />
+            <Route path="/login"         element={<Login />} />
+            <Route path="/register"      element={<Register />} />
           </Routes>
         </div>
       </Router>
     </AuthProvider>
   );
 }
-
-export default App;
