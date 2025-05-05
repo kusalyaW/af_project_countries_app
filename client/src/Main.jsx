af84ad0afea369728d8e1d89d4cedf20722f65d5
kusalya <kusalyawidanagama@gmail.com>
2025-05-05 22:06:41 +0530

    Update client main app

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+useEffect(() => {
+  console.log('🚀 API_BASE_URL =', import.meta.env.VITE_API_BASE_URL);
+}, []);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
