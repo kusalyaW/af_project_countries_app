a16b9fe841fba89fe769dddd197247611bb682b8
kusalya <kusalyawidanagama@gmail.com>
2025-05-06 00:07:19 +0530

    Update client main2

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,17 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-function Root() {
-  useEffect(() => {
-    console.log('🚀 API_BASE_URL =', import.meta.env.VITE_API_BASE_URL);
-  }, []);
-
-  return <App />;
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>
 );
