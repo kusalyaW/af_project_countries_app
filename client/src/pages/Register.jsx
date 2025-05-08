7f9df26596888bc36f6658ccffdcde495bc2bc69
kusalya <kusalyawidanagama@gmail.com>
2025-05-09 02:52:47 +0530

    Update reg

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -53,19 +53,26 @@ export default function Register() {
           </div>
         )}
 
-        {/* Name Field */}
+        
         <div className="relative">
           <UserIcon className="w-5 h-5 text-gray-400 dark:text-gray-500 absolute left-3 top-1/2 -translate-y-1/2" />
           <input
-            type="text"
-            placeholder="Full Name"
-            value={name}
-            onChange={e => setName(e.target.value)}
-            className="w-full pl-10 pr-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-          />
+  type="text"
+  placeholder="Full Name"
+  value={name}
+  onChange={e => setName(e.target.value)}
+  className="
+    w-full pl-10 pr-3 py-2 border rounded
+    text-gray-900 placeholder-gray-500
+    dark:text-gray-100 dark:placeholder-gray-400
+    focus:outline-none focus:ring-2 focus:ring-blue-400
+    dark:bg-gray-700 dark:border-gray-600
+  "
+/>
+
         </div>
 
-        {/* Email Field */}
+        
         <div className="relative">
           <EnvelopeIcon className="w-5 h-5 text-gray-400 dark:text-gray-500 absolute left-3 top-1/2 -translate-y-1/2" />
           <input
@@ -73,11 +80,16 @@ export default function Register() {
             placeholder="Email Address"
             value={email}
             onChange={e => setEmail(e.target.value)}
-            className="w-full pl-10 pr-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-          />
+            className="
+            w-full pl-10 pr-3 py-2 border rounded
+            text-gray-900 placeholder-gray-500
+            dark:text-gray-100 dark:placeholder-gray-400
+            focus:outline-none focus:ring-2 focus:ring-blue-400
+            dark:bg-gray-700 dark:border-gray-600
+          "          />
         </div>
 
-        {/* Password Field */}
+        
         <div className="relative">
           <LockClosedIcon className="w-5 h-5 text-gray-400 dark:text-gray-500 absolute left-3 top-1/2 -translate-y-1/2" />
           <input
@@ -85,8 +97,13 @@ export default function Register() {
             placeholder="Password"
             value={password}
             onChange={e => setPassword(e.target.value)}
-            className="w-full pl-10 pr-10 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-          />
+            className="
+            w-full pl-10 pr-10 py-2 border rounded
+            text-gray-900 placeholder-gray-500
+            dark:text-gray-100 dark:placeholder-gray-400
+            focus:outline-none focus:ring-2 focus:ring-blue-400
+            dark:bg-gray-700 dark:border-gray-600
+          "          />
           <button
             type="button"
             onClick={() => setShowPwd(!showPwd)}
@@ -98,7 +115,7 @@ export default function Register() {
           </button>
         </div>
 
-        {/* Submit Button */}
+       
         <button
           type="submit"
           disabled={loading}
