cbb38085e5aeb478f3d8ccc7eb6fad4e8a282714
kusalya <kusalyawidanagama@gmail.com>
2025-05-09 01:58:29 +0530

    Update footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,8 +43,9 @@ export default function App() {
             <Route path="/login"         element={<Login />} />
             <Route path="/register"      element={<Register />} />
             <Route path="/country/:code" element={<Country />} />
-            <Route element={<Layout />}>
             <Route path="/app"      element={<Home />} />
+            <Route element={<Layout />}>
+            
             
             
             <Route path="/favorites"     element={<Favorites />} />
