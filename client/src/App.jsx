81ce594cc42559d416ad22c8f3753a5866c40897
kusalya <kusalyawidanagama@gmail.com>
2025-05-09 01:56:39 +0530

    Update side2hmeeappfoot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,10 +42,10 @@ export default function App() {
             <Route path="/"         element={<Landing />} />
             <Route path="/login"         element={<Login />} />
             <Route path="/register"      element={<Register />} />
-          
+            <Route path="/country/:code" element={<Country />} />
             <Route element={<Layout />}>
             <Route path="/app"      element={<Home />} />
-            <Route path="/country/:code" element={<Country />} />
+            
             
             <Route path="/favorites"     element={<Favorites />} />
             
